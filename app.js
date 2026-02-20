24bb01d086c8647dad4626bbc14328c686e995eb
Samuel Adeleke <sadeleke745@gmail.com>
2026-02-20 10:04:33 +0100

    feat: handle form data

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 import e from "express";
 import aboutRoutes from "./about.js";
 const app = e();
+app.use(e.json());
 const PORT = 3000;
-
 app.set("view engine", "ejs");
+
+// middleware to parse form data
+app.use(e.urlencoded({ extended: true }));
+
+app.post("/submit-form", (req, res) => {
+  console.log(req.body);
+
+  const { name, email } = req.body;
+
+  res.send(`Form submitted by: ${name}, Email: ${email}`);
+});
 
 // home route
 app.get("/", (req, res) => {
   res.send("Welcome to our EXPRESS.JS server!");
 });
 
+app.get("/profile", (req, res) => {
+  // reading request headers
+  const userAgent = req.get("User-agent");
+  console.log(userAgent);
+  res.set("X-Custom-Header", "HelloWorld");
+  res.status(200).send("Headers handled!");
+});
+
+app.use("/about", aboutRoutes);
 // app.get("/about", (req, res) => {
 //   res.json({
 //     message: "About Us",
@@ -17,14 +37,17 @@ app.get("/", (req, res) => {
 //   });
 // });
 
-app.use("/about", aboutRoutes);
-
 app.get("/contact", (req, res) => {
   res.status(200).json({ message: "Contact Us", email: "contact@example.com" });
 });
 
 app.get("/user", (req, res) => {
   res.render("index", { username: "John Doe", email: "johndoe@mail.com" });
+});
+
+app.get("/search", (req, res) => {
+  const query = req.query.query;
+  res.send(`You searched for: ${query}`);
 });
 
 // start the server
