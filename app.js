6a6c3f5759bc026cb61d842c0d2f922b3df9abbb
Samuel Adeleke <sadeleke745@gmail.com>
2026-02-20 10:43:45 +0100

    feat: add file upload with multer

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import e from "express";
+import multer from "multer";
+import fs from "fs";
+
 import aboutRoutes from "./about.js";
+
 const app = e();
 app.use(e.json());
+
 const PORT = 3000;
+
 app.set("view engine", "ejs");
 
 // middleware to parse form data
@@ -14,6 +20,17 @@ app.post("/submit-form", (req, res) => {
   const { name, email } = req.body;
 
   res.send(`Form submitted by: ${name}, Email: ${email}`);
+});
+
+app.post("/json-data", (req, res) => {
+  console.log(req.body);
+  const { userId, message } = req.body;
+
+  res.status(200).json({
+    status: "success",
+    userId: userId,
+    message: message,
+  });
 });
 
 // home route
@@ -48,6 +65,34 @@ app.get("/user", (req, res) => {
 app.get("/search", (req, res) => {
   const query = req.query.query;
   res.send(`You searched for: ${query}`);
+});
+
+// file upload with multer
+// first set up multer storage configuration
+const uploadDir = "./uploads";
+// Check if the directory exists, if not, create it
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads"); // Folder to store uploaded files
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Generate the unique filename
+  },
+});
+
+// create multer instance with file size limit (10MB)
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).send(`File uploaded: ${req.file.filename}`);
 });
 
 // start the server
